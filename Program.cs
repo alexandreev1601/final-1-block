1feed24e49e9588359bd1fee28c97df4312517a8
Alexandr Andreev <alexandreev1601@gmail.com>
2024-05-06 09:52:12 +0500

    create foreach

--- Program.cs
+++ Program.cs
@@ -2,3 +2,17 @@
 
 string[] array = { "a", "bb", "ccc", "dddd", "eeeee", "z" }; 
 var result = new string[array.Length];
+
+var realSize = 0;
+
+foreach (var value in array) {
+
+  if (value.Length <= 3) {
+
+    result[realSize] = value;
+
+    realSize++;
+
+  }
+
+}
